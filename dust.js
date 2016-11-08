6cf1ebd7260e2730e9945a6a80dbffc1562220c6
Kumaresh C J <cjkumaresh@gmail.com>
2016-11-08 15:16:25 +0530

    Create dust.js

---
+++ dust.js
@@ -0,0 +1,35 @@
+{references/}
+{#section/}
+{?exists/}
+{^not_exists/}
+{@helper/}	
+{! Comments	!}
+	context-helpers or handlers
+		using the logic in the context rather than using in template itself
+	dustjs-helpers
+		{@eq}: strictly equal to
+		{@ne}: not strictly equal to
+		{@gt}: greater than
+		{@lt}: less than
+		{@gte}: greater than or equal to
+		{@lte}: less than or equal to
+{<inline_partial/}
+{>partial/}
+{~special}
+	{~s} becomes a single space
+	{~n} becomes a new line
+	{~r} becomes a carriage return
+	{~lb} becomes a left curly brace {
+	{~rb} becomes a right curly brace }
+{filters|s}
+	h – HTML-encode
+	s – turn off automatic HTML encoding
+	j – Javascript string encode
+	u – encodeURI
+	uc – encodeURIComponent
+	js – JSON.stringify
+	jp – JSON.parse	
+  
+{@contextDump/} - to print the context for debugging
+ 
+	
